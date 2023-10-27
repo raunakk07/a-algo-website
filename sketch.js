442404c42485fce690b24544fdc51ad58de50686
Harshit Srivastava <raunak0719@gmail.com>
2023-10-27 21:31:53 +0530

    adding some variables

--- sketch.js
+++ sketch.js
@@ -2,11 +2,18 @@ var cols = 5;
 var rows = 5;
 var grid = new Array(cols);
 
-function Spot(){
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
-  
+var openList = [];
+var closedList = [];
+
+var start;
+var end;
+class Spot {
+  constructor() {
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+
+  }
 }
 
 function setup() {
@@ -18,11 +25,12 @@ function setup() {
   }
   
   for(var a =0;i<cols;i++){
-    for(var b =0;i<ros;i++){
-    grid[i][j] = new Spot(); 
+    for(var b =0;i<rows;i++){
+      grid[i][j] = new Spot(); 
+    }
   }
-  }
-  
+  start = grid[0][0];
+  end = grid[cols-1][rows-1];
   console.log(grid);
 }
 
